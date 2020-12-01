81fe0e09de6a95a869ef312fff3b524db3bf703e
svg94 <liga-2000@hotmail.de>
2020-12-02 00:08:14 +0100

    Started EvalController. Finished outer part from main.js. Controller inside still empty.

--- src/controller/EvaluationController.js
+++ src/controller/EvaluationController.js
@@ -1,3 +1,35 @@
-function Evaluationcontroller(){
+var eval = require("../model/Evaluationrecord");
+class Evaluationcontroller{
+    constructor(mongo) {
+        this.dbManager = mongo;
+        this.url = "mongodb://localhost:27017/";
+    }
+    EvalByID(Sid){
+        /*  TODO: Rewrite for EvalRecords.
+        var fetched={
+            firstname: "",
+            lastname: "",
+            id: 0,
+        };
+        var promise = new Promise(((resolve, reject) => {
+            this.dbManager.connect(this.url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("highperformance");
+                dbo.collection("Salesmen").findOne({id : Number(Sid)}, function(err, result) {
+                    if (err) throw err;
+                    console.log("Fetched",result);
+                    fetched.firstname = result.firstname;
+                    fetched.lastname = result.lastname;
+                    fetched.id = result.id;
+                    resolve(result);
+                    db.close();
+                })
+            });
+        }));
+        return fetched;*/
+    }
+    PostEval(evalrecord, sid){
 
+    }
 }
+module.exports ={Evaluationcontroller};
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
-const salesmen = require('./model/Salesman');
 const sController = require('./controller/SalesmenController');
-var MongoClient = require('mongodb').MongoClient;
+const eController = require('./controller/EvaluationController');
+const MongoClient = require('mongodb').MongoClient;
 const express=require('express');
 const app=express();
 app.use(express.json());
 var sControl = new sController.Salesmencontroller(MongoClient);
+var eControl = new eController.Evaluationcontroller(MongoClient);
 
 app.get('/',(req,res)=>{
     res.send("Welcome to Node-based REST-API");
 });
 
 app.get('/Salesman/:id',(req, res) => {
-    let fetchedsalesman = new Promise((resolve, reject) => {
+    let fetchedsalesman = new Promise((resolve) => {
         let fetched = sControl.salesmanByID(req.params.id);
-        setTimeout(()=>{resolve(fetched);},100);            //Is there another way than using a timer?
+        setTimeout(()=>{resolve(fetched);},20);            //Is there another way than using a timer?
     });
     fetchedsalesman.then(
         function (value){
@@ -32,20 +33,24 @@ app.post("/Salesman",((req, res) => {
     res.send("Stored Salesman in database");
 }));
 
+app.get('/Evaluationrecord/:id',((req, res) => {
+    let fetchedEval = new Promise((resolve) => {
+        let fetched = eControl.EvalByID(req.params.id);
+        setTimeout(()=>{resolve(fetched);},20);            //Is there another way than using a timer?
+    });
+    fetchedEval.then(
+        function (value){
+            console.log(value);
+            res.send(value);
+        },
+        function (err){
+            res.send(err);
+        }
+    )
+}));
+
 app.listen(8081,()=>{
     console.log('Application started.');
 });
-/*app.get('/addS',(req,res)=>{
-    let salesPromise = new Promise(function(resolve,reject){
-        var s= new salesmen.Salesman("Lara","Loft",77);
-        resolve(s);
-    });
-    salesPromise.then(s =>{
-        dbo.collection("Salesmen").insertOne(s,function(err,resp){
-            if(err) throw err;
-            res.send("Salesmen sent.");
-            database.close();
-        });
-    });
-});*/
+
 
